1ef0031dbbac955e699355d98b19b74996f510e7
shanth923 <shanth923@gmail.com>
2020-02-21 17:01:03 +0530

    Initial Commit

---
+++ TrafficSignalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSignalApp
+//
+//  Created by apple on 21/02/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
