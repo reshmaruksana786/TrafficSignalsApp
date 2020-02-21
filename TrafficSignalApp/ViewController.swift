4a18d1bca0f5aeb58b018af9a100a497479d2f2d
shanth923 <shanth923@gmail.com>
2020-02-21 18:11:21 +0530

    Traffic signals

--- TrafficSignalApp/ViewController.swift
+++ TrafficSignalApp/ViewController.swift
@@ -9,12 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var red: UILabel!
+    @IBOutlet weak var viewcontent: UIView!
+    
+    @IBOutlet weak var orange: UILabel!
+    @IBOutlet weak var green: UILabel!
     override func viewDidLoad() {
+
+        red.layer.cornerRadius = red.frame.width/2
+        red.clipsToBounds = true
+        
+        orange.layer.cornerRadius = orange.frame.width/2
+        orange.clipsToBounds = true
+        
+        green.layer.cornerRadius = green.frame.width/2
+        green.clipsToBounds = true
+        
+        self.red.isHidden = true
+        self.orange.isHidden = true
+        self.green.isHidden = true
+        
+        self.stop.isHidden = true
+        self.ready.isHidden = true
+        self.go.isHidden = true
+      
+
+       
+        
+        Timer.scheduledTimer(withTimeInterval: 11, repeats: true) { timer in
+            
+            
+                     
+                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    
+                           self.red.isHidden = false
+                           self.orange.isHidden = true
+                           self.green.isHidden = true
+                    
+                    
+                           self.stop.isHidden = false
+                           self.ready.isHidden = true
+                           self.go.isHidden = true
+                    
+                  
+                 }
+                     
+                     DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+                        
+                        
+                         self.red.isHidden = true
+                         self.orange.isHidden = false
+                         self.green.isHidden = true
+                        
+                        
+                        self.stop.isHidden = true
+                        self.ready.isHidden = false
+                        self.go.isHidden = true
+                      
+                     }
+                     DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                        
+                         self.red.isHidden = true
+                         self.orange.isHidden = true
+                         self.green.isHidden = false
+                        
+                        self.stop.isHidden = true
+                        self.ready.isHidden = true
+                        self.go.isHidden = false
+                        
+                        
+                      
+                     }
+
+        }
+    
+        
+
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+  
 
-
+    @IBOutlet weak var stop: UILabel!
+    
+    @IBOutlet weak var go: UILabel!
+    @IBOutlet weak var ready: UILabel!
+    
 }
 
